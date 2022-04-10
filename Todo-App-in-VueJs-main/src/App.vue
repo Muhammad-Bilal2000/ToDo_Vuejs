7bf10184d6e9d523669e888a1c9d0f46c1314493
Muhammad-Bilal2000 <82469386+Muhammad-Bilal2000@users.noreply.github.com>
2022-04-10 15:16:37 +0500

    Add files via upload

---
+++ Todo-App-in-VueJs-main/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Login/>
+    <Home/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/Login.vue'
+import Home from './components/Home.vue'
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    Login,
+    Home
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
